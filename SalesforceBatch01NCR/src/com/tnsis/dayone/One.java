f6d82f6143ab10bfe5b1da2f9ab7812d7729dcc4
Muskan <katariamuskan02@gmail.com>
2024-03-14 13:10:36 +0530

    Initial Commit.

---
+++ SalesforceBatch01NCR/src/com/tnsis/dayone/One.java
@@ -0,0 +1,10 @@
+package com.tnsis.dayone;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SalesforceBatch01NCR/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SalesforceBatch01NCR {
+}
